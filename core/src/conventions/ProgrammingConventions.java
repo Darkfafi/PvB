9ce8cd583477a63da7e914cdd82cf081cad0586d
Ramses di Perna <15826@ma-web.nl>
2017-04-06 11:03:14 +0200

    updated conventions.

--- core/src/conventions/ProgrammingConventions.java
+++ core/src/conventions/ProgrammingConventions.java
@@ -15,11 +15,17 @@ final class ProgrammingConventions
 		ItemTwo
 	}
 	
-	public static final String MY_FINAL_STRING = "FinalString";
+	public static final String MY_STATIC_FINAL_STRING = "StaticFinalString";
+	
+	public static String ThisIsMyPublicStaticString = "StaticString";
 	
 	public int MyPublicInteger = 1;
 	
+	protected static float thisIsMyProtectedStaticFloat = 0.5f;
+	
 	protected float myProtectedFloat = 0.1f;
+	
+	private static float thisIsMyPrivateStaticFloat = 0.5f;
 	
 	private float _myPrivateFloat = 0.1f;
 	
@@ -28,6 +34,15 @@ final class ProgrammingConventions
 		
 	}
 	
+	public static void publicStaticVoidMethod(float paramOne)
+	{
+		
+	}
+	
+	/**
+	 * This is a summary
+	 * @param paramOne represents foo
+	 */
 	public void publicVoidMethod(int paramOne)
 	{
 		
@@ -39,9 +54,29 @@ final class ProgrammingConventions
 	// RULE: Setters are named 'set' + [variable name] and are written in one line.
 	public void setMyPrivateFloat(float value) {_myPrivateFloat = value;}
 	
+	
+	protected static void protectedStaticVoidMethod(float paramOne)
+	{
+		privateStaticVoidMethod(thisIsMyPrivateStaticFloat, 2f);	
+	}
+	
 	protected void protectedVoidMethod()
 	{
 		privateVoidMethod();
+	}
+	
+	private static void privateStaticVoidMethod(double paramOne, float paramTwo)
+	{
+		
+	}
+	
+	/**
+	 * Generic method summary
+	 */
+	@SuppressWarnings("unused") // Only added to remove warnings for the conventions class. Do not use these without good reason!
+	private <T extends BaseClass> void privateGenericMethod()
+	{
+		
 	}
 	
 	private void privateVoidMethod()
@@ -50,7 +85,6 @@ final class ProgrammingConventions
 	}
 	
 	// Rule: All names for abstract classes start with the term 'Base'
-	@SuppressWarnings("unused") // Only added to remove warnings for the conventions class. Do not use these without good reason!
 	private abstract class BaseClass implements IMyInterface
 	{
 		public void InterfaceMethod()
@@ -66,4 +100,20 @@ final class ProgrammingConventions
 	{
 		void InterfaceMethod();
 	}
+	
+	/* Commented for error handling purposes.
+	public class MySingletonClass
+	{	
+		private static MySingletonClass _instance;
+		
+		// RULE: Always give a singleton a static getInstance(); method.
+		public static MySingletonClass getInstance()
+		{
+			if(_instance == null)
+				_instance = new MySingletonClass();
+			
+			return _instance;
+		}
+	}
+	*/
 }
